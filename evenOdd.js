3399ca7417e80560d084f3409392709d189f685a
Lokesh Kumar <lokeshkumarprince7@gmail.com>
2020-10-15 13:10:50 +0200

    added comment

--- evenOdd.js
+++ evenOdd.js
@@ -2,7 +2,7 @@ showNumbers(10);
 
 function showNumbers(limit){
     for (let i =0; i<= limit; i++){
-        const message = (i%2 === 0) ? 'Even' : 'Odd';
+        const message = (i%2 === 0) ? 'Even' : 'Odd'; // conditional operator
         console.log(i , message);
     }
 } 
